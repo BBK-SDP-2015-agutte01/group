80522ebe2d6154da398d279618f98ce88563fb06
Arron (iMac) <info@arronhormoz.com>
2015-04-23 13:02:05 +0100

    Completed TODOs
    Following the Todos, made the Coordinator object an actor rather than
    having it as as separate class. Also made the set method a message &
    message handler.

--- src/main/scala/Coordinator.scala
+++ src/main/scala/Coordinator.scala
@@ -1,11 +1,18 @@
 import akka.actor.{Actor, Props}
 
-// TODO
-//
-// Make this an actor and write a message handler for at least the
-// set method.
-//
-object Coordinator {
+object Coordinator extends Actor{
+
+  def receive = {
+    case Start => {
+      val test = context.actorOf(Props[Calculator], "cal")
+      test ! Calculate(34)
+    }
+    case Set(x: Int, y: Int, c: Colour) => {
+      image(x, y) = c
+      waiting -= 1
+    }
+  }
+
   def init(im: Image, of: String) = {
     image = im
     outfile = of
@@ -17,25 +24,12 @@ object Coordinator {
   var outfile: String = null
   var image: Image = null
 
-  // TODO: make set a message
-  def set(x: Int, y: Int, c: Colour) = {
-    image(x, y) = c
-    waiting -= 1
-  }
-
   def print = {
     assert(waiting == 0)
     image.print(outfile)
   }
 }
 
-class CoordinatorActor extends Actor {
-  def receive = {
-    case Start => {
-      val test = context.actorOf(Props[Calculator], "cal")
-      test ! Calculate(34)
-    }
-  }
-}
 case class Start()
 case class Calculate(row: Int)
+case class Set(x: Int, y: Int, c: Colour)
