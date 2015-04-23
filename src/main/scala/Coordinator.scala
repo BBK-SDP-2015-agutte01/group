90300fdfb9ad53eb7cb791522a715d3dd450f3f1
Arron (iMac) <info@arronhormoz.com>
2015-04-23 13:38:29 +0100

    Revert "Completed TODOs"
    This reverts commit 80522ebe2d6154da398d279618f98ce88563fb06.

--- src/main/scala/Coordinator.scala
+++ src/main/scala/Coordinator.scala
@@ -1,18 +1,11 @@
 import akka.actor.{Actor, Props}
 
-object Coordinator extends Actor{
-
-  def receive = {
-    case Start => {
-      val test = context.actorOf(Props[Calculator], "cal")
-      test ! Calculate(34)
-    }
-    case Set(x: Int, y: Int, c: Colour) => {
-      image(x, y) = c
-      waiting -= 1
-    }
-  }
-
+// TODO
+//
+// Make this an actor and write a message handler for at least the
+// set method.
+//
+object Coordinator {
   def init(im: Image, of: String) = {
     image = im
     outfile = of
@@ -24,12 +17,25 @@ object Coordinator extends Actor{
   var outfile: String = null
   var image: Image = null
 
+  // TODO: make set a message
+  def set(x: Int, y: Int, c: Colour) = {
+    image(x, y) = c
+    waiting -= 1
+  }
+
   def print = {
     assert(waiting == 0)
     image.print(outfile)
   }
 }
 
+class CoordinatorActor extends Actor {
+  def receive = {
+    case Start => {
+      val test = context.actorOf(Props[Calculator], "cal")
+      test ! Calculate(34)
+    }
+  }
+}
 case class Start()
 case class Calculate(row: Int)
-case class Set(x: Int, y: Int, c: Colour)
